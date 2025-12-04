53d8cae6033dd66172d525b5394fff936a39bbfb
the-chivo <titomonguer@gmail.com>
2025-12-04 22:13:08 +0100

    Update Entrenador.java

--- T5/Proyecto_Final/Entrenador.java
+++ T5/Proyecto_Final/Entrenador.java
@@ -1,19 +1,23 @@
 
-package com.mycompany.addt4ef;
+*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.addt4ef1;
 
 public class Entrenador {
 
     private int id;
     private String nombre;
     private String raza;
-    private int n_partidos;
-    private String equipo;
+    private int partidos;
 
     // Constructor
 
-    public Entrenador(String nombre, String equipo) {
-        this.nombre = nombre;
-        this.equipo = equipo;
+    public Entrenador(String nombre, String raza, int partidos) {
+        this.nombre = nombre;          
+        this.raza = raza;
+        this.partidos = partidos;
     }
 
     // Getters y Setters
@@ -22,17 +26,11 @@ public class Entrenador {
 
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
-
-    public String getEquipo() { return equipo; }
-    public void setEquipo(String equipo) { this.equipo = equipo; }
-
-    public int getPartidos() { return n_partidos; }
-    public void setPartidos(int n_partidos) { this.n_partidos = n_partidos; }
+   
+    public String getRaza() { return raza; }
+    public void setRaza(String raza) { this.raza = raza; }
+    
+    public int getPartidos() { return partidos; }
+    public void setPartidos(int n_partidos) { this.partidos = n_partidos; }
+    
 }
-
-
-
-
-
-
-
